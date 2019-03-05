24d4b5a18ac26b3ce3ec2d26cfe07e93287e5b17
Sergey Baikov <rayleonard9@gmail.com>
2019-03-05 12:00:07 +0300

    bishop move

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,50 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+/**
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell position) {
+        this.position = position;
+    }
+
+    @Override
+    public Cell position() {
+        return this.position;
+    }
+
+    @Override
+    public Cell[] way(Cell source, Cell dest) {
+        int startX = source.x;
+        int startY = source.y;
+        Cell[] steps = new Cell[0];
+        if (isDiagonal(source, dest)) {
+            steps = new Cell[dest.x - source.x];
+            for (int i = 0; startX == dest.x && startY == dest.y; i++) {
+                dest.x - 1;
+            }
+        }
+        return steps;
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        boolean result = false;
+        if (Math.abs(dest.x - source.x) == Math.abs(dest.y - source.y)) {
+            result = true;
+        }
+        return result;
+    }
+}
